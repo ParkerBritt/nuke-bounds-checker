6342526d10d44e293a0fd20bf3a6b18290e0813c
Parker <parker@parkerbritt.com>
2025-06-11 17:31:45 +0100

    feat: find responsible node

--- script.py
+++ script.py
@@ -5,18 +5,44 @@ nuke.root().begin()
 allNodes = nuke.root().nodes()
 # TODO: check for no nodes
 
-maxBoundDist = 200
 
 # TODO: Sort by heirarcy instead of height
 
-def checkOutOfBounds(startNode : nuke.Node) -> bool:
-    bbox = curNode.bbox()
-    return bbox.x()<-maxBoundDist or bbox.y()<-maxBoundDist or (bbox.x()+bbox.w())>(node.width()+maxBoundDist) or (bbox.y()+bbox.h())>(node.height()+maxBoundDist)
+def findTargetNode(startNode : nuke.Node | None) -> nuke.Node:
+    # TODO: handle nonetype inputs
+
+    # find parent responsible for out of bounds box
+    prevNode = startNode
+    foundTarget = False
+    while not foundTarget:
+        curNode = prevNode.input(0)
+
+        # if not out of bounds then the previous node is the top level target
+        if(not checkOutOfBounds(curNode)):
+            return prevNode
+
+
+        prevNode = curNode
+
+def checkOutOfBounds(node : nuke.Node) -> bool:
+    maxBoundDist = 200
+
+    bbox = node.bbox()
+    xMin = bbox.x()<-maxBoundDist 
+    yMin = bbox.y()<-maxBoundDist
+
+    xMax = (bbox.x()+bbox.w())>(node.width()+maxBoundDist)
+    yMax = (bbox.y()+bbox.h())>(node.height()+maxBoundDist)
+    status = xMin or yMin or xMax or yMax 
+    print(bbox.x())
+    return status
 
 
 
 
+print("STATUS:", checkOutOfBounds(nuke.toNode("CheckerBoard1")))
 for curNode in allNodes:
+    print("\n----\n")
     print("name:", curNode.name())
     print(bbox.x(), bbox.y(), bbox.w(), bbox.h())
     print(curNode.width(), curNode.height())
@@ -27,12 +53,13 @@ for curNode in allNodes:
     if(isReformatTarget):
         # print(nuke.getColor(curNode.knob("tile_color").value()))
         curNode.knob("tile_color").setValue(4278190335)
+
         # TODO: filter only connected nodes
-        outputs = curNode.dependencies()
-        print("outputs:", outputs)
+
         curNode.setSelected(True)
+        print("target node:", findTargetNode(curNode))
         reformatNode = nuke.createNode("Reformat")
-        break
+        # break
         print("color to red")
     else:
         curNode.knob("tile_color").setValue(536805631)
