990cde6300092ce6c152c3eb7eb3828680368c3d
Parker <parker@parkerbritt.com>
2025-06-13 14:36:42 +0100

    feat: working mvp

--- script.py
+++ script.py
@@ -12,17 +12,66 @@ def findTargetNode(startNode : nuke.Node) -> nuke.Node:
     # TODO: handle nonetype inputs
 
     # find parent responsible for out of bounds box
-    prevNode = startNode
     foundTarget = False
-    while not foundTarget:
-        curNode = prevNode.input(0)
+
+    vistedNodes = list()
+    traversalBuffer = list()
+    traversalBuffer.append(startNode)
+
+    problemNodes = list()
+
+    while len(traversalBuffer) != 0:
+        print("len:", len(traversalBuffer))
+        curNode = traversalBuffer.pop()
+        print("current node", curNode.name())
+        print("inputs:", curNode.inputs())
+        for i in range(curNode.inputs()):
+            print("i:", i)
+            inputNode = curNode.input(i)
+
+            if(inputNode is not None):
+                print("adding:", inputNode.name())
+                traversalBuffer.append(inputNode)
+                vistedNodes.append(inputNode)
+            else:
+                print("skipping")
+
+    print("done")
+    vistedNodes.reverse()
+    print(vistedNodes)
+
+    nukescripts.clear_selection_recursive()
+    
+    for curNode in vistedNodes:
+
+        print("current node:", curNode.name())
+        curNode.redraw()
+        isReformatTarget = checkOutOfBounds(curNode)
+
+
+
+        if(isReformatTarget):
+            # print(nuke.getColor(curNode.knob("tile_color").value()))
+            curNode.knob("tile_color").setValue(4278190335)
+
+            # TODO: filter only connected nodes
+            print("selected nodes:", nuke.selectedNodes())
+
+            curNode.setSelected(True)
+            reformatNode = nuke.createNode("Reformat")
+            reformatNode.setSelected(False)
+            # curNode.setSelected(False)
+            # break
+            print("color to red")
+        else:
+            curNode.knob("tile_color").setValue(536805631)
 
         # if not out of bounds then the previous node is the top level target
-        if(not checkOutOfBounds(curNode)):
-            return prevNode
+        # if(not checkOutOfBounds(curNode)):
+        #     problemNodes.append(prevNode)
 
 
-        prevNode = curNode
+
 
 def checkOutOfBounds(node : nuke.Node) -> bool:
     maxBoundDist = 200
@@ -40,7 +89,7 @@ def checkOutOfBounds(node : nuke.Node) -> bool:
 
 
 
-print("responsible node:", findTargetNode(nuke.toNode("Merge5")).name())
+print("responsible node:", findTargetNode(nuke.toNode("Fixer")))
 # for curNode in allNodes:
 #     print("\n----\n")
 #     print("name:", curNode.name())
@@ -58,7 +107,7 @@ print("responsible node:", findTargetNode(nuke.toNode("Merge5")).name())
 
 #         curNode.setSelected(True)
 #         print("target node:", findTargetNode(curNode))
-#         reformatNode = nuke.createNode("Reformat")
+#         # reformatNode = nuke.createNode("Reformat")
 #         # break
 #         print("color to red")
 #     else:
